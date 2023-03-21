17268d69c36d494086a80f0cd77153edeb8aee57
Sunil Babu <91570349+sunilprasanna@users.noreply.github.com>
2023-03-21 11:03:48 -0500

    git update

--- release-branch.sh
+++ release-branch.sh
@@ -15,6 +15,7 @@ git checkout development
 echo ${RELEASE_VERSION} > version.txt
 
 # Stage only version file, commit and push to remote
+git remote set-url origin https://sunilprasanna:$MERGE_TOKEN@github.com/sunilprasanna/sunilprasanna.git 
 git add version.txt
 git commit -m "chore: update to version ${RELEASE_VERSION}"
 git push -u origin development -f
