a7d1e570cd6c937973cfab7320ee04f9d5e2571a
Sunil Babu <91570349+sunilprasanna@users.noreply.github.com>
2023-07-27 09:19:15 -0500

    Add trello card github actions

---
+++ .github/workflows/addTrelloCardsForReleaseAction/trelloCreateCard.js
@@ -0,0 +1,31 @@
+const fetch = require("node-fetch");
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const newCardName = core.getInput("who-to-greet");
+  console.log(`New Card Name: ${newCardName}`);
+  const trelloKey = core.getInput("trello-api-key");
+  const trelloToken = core.getInput("trello-api-token");
+  const trelloAPIUrl = `https://api.trello.com/1/cards?idList=64badf32182ac7d928d9304f&key=${trelloKey}&token={trelloToken}`;
+  console.log(`Trello API URL: ${trelloAPIUrl}`);
+  fetch(trelloAPIUrl, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      console.log(`Response: ${response.status} ${response.statusText}`);
+      return response.text();
+    })
+    .then((text) => console.log(text))
+    .catch((err) => console.error(err));
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
