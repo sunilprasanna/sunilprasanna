b631fd38b04f8f68b9bdf62a93f78ec508ae7044
Sunil Babu <91570349+sunilprasanna@users.noreply.github.com>
2023-08-16 16:08:33 -0500

    Update trelloCreateCard.js

--- .github/workflows/addTrelloCardsForReleaseAction/trelloCreateCard.js
+++ .github/workflows/addTrelloCardsForReleaseAction/trelloCreateCard.js
@@ -3,17 +3,52 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 
 try {
-  const newCardName = core.getInput("name-of-card");
-  console.log(`New Card Name: ${newCardName}`);
-  const cardDescription = core.getInput("description-of-card");
-  const PRLink = core.getInput("pr-link");
+  const milestoneLabel = core.getInput("milestone-label")
+  let labelId = null;
+  const baseUrl = "https://api.trello.com/1/"
+  const boardId = "64badf32182ac7d928d9304f"
   const trelloKey = core.getInput("trello-api-key");
   const trelloToken = core.getInput("trello-api-token");
-  const baseUrl =
-    "https://api.trello.com/1/cards?idList=64badf32182ac7d928d9304f";
-  const trelloAPIUrl = `${baseUrl}&name=${newCardName}&desc=${cardDescription}&urlSource=${PRLink}&key=${trelloKey}&token=${trelloToken}`;
-  console.log(`Trello API URL: ${trelloAPIUrl}`);
-  fetch(trelloAPIUrl, {
+  const tokenQueryStrings = `&key=${trelloKey}&token=${trelloToken}`
+  const trelloAPIBoardUrl = `${baseUrl}boards/${boardId}/labels?fields=id&fields=name${tokenQueryStrings}`
+  const trelloAPILabelUrl = `${baseUrl}labels?name=${milestoneLabel}&color=blue&idBoard=${boardId}${tokenQueryStrings}`
+  fetch(trelloAPIBoardUrl, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  })
+      .then((response) => {
+        console.log(`Response: ${response}`);
+        if(response.some(item => item.name === milestoneLabel)){
+          labelId = item.id
+        }
+        else{
+          fetch(trelloAPICardUrl, {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+            },
+          })
+              .then((response) => {
+                console.log(`Response: ${response}`);
+                labelId = response.id;
+                return response.text();
+              })
+              .then((text) => console.log(text))
+              .catch((err) => console.error(err));
+        }
+      })
+      .then((text) => console.log(text))
+      .catch((err) => console.error(err));
+  const newCardName = core.getInput("name-of-card");
+  const cardDescription = core.getInput("description-of-card");
+  const PRLink = core.getInput("pr-link");
+  const baseCardUrl =
+      `${baseUrl}cards?idList=${boardId}`;
+  const trelloAPICardUrl = `${baseCardUrl}&name=${newCardName}&desc=${cardDescription}&urlSource=${PRLink}${tokenQueryStrings}`;
+  console.log(`Trello API URL: ${trelloAPICardUrl}`);
+  fetch(trelloAPICardUrl, {
     method: "POST",
     headers: {
       Accept: "application/json",
